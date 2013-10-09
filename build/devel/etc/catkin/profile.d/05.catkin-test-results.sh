1a550b0d45ccb8467e689ff984d7394612c39008
Peng Hou <penghou620@gmail.com>
2013-10-09 02:00:30 -0400

    Clean Up

--- build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/penghou/Documents/ros/groovy/color_detection/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
