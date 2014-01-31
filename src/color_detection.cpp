d838dd7cb7d2999ac50cac5ca5fc4ce4afea848e
Peng Hou <dustsnow@gmail.com>
2014-01-31 16:45:29 -0500

    Change package name to match the folder name so that launch file can be actually launched; Change default color value to more broad range

--- src/color_detection.cpp
+++ src/color_detection.cpp
@@ -20,9 +20,9 @@ static const char WINDOW[] = "Image Processed";
 //Use method of ImageTransport to create image publisher
 image_transport::Publisher pub;
 
-int LowerH = 160;
-int LowerS = 52;
-int LowerV = 139;
+int LowerH = 0;
+int LowerS = 0;
+int LowerV = 0;
 int UpperH = 180;
 int UpperS = 196;
 int UpperV = 170;
@@ -150,7 +150,7 @@ int main(int argc, char **argv)
     * subscribe() returns an image_transport::Subscriber object, that you must hold on to until you want to unsubscribe. 
     * When the Subscriber object is destructed, it will automatically unsubscribe from the "camera/image_raw" base topic.
     */
-    //    image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, imageCallback);
+    //image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, imageCallback);
     image_transport::Subscriber sub = it.subscribe("camera/image_raw", 1, colorDetectionCallback);
     //OpenCV HighGUI call to destroy a display window on shut-down.
     cv::destroyWindow(WINDOW);
